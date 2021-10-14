1ecf785307c8eeb7d15b0dd4d57866f808fa417c
Charles Havener <charles.havener@gmail.com>
2021-10-14 14:16:12 -0700

    .\main.py

---
+++ main.py
@@ -0,0 +1,22 @@
+import tkinter as tk
+from lib.app import App
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    root.title("Floor Test Report Generator")
+
+    # Simply set the theme
+    root.tk.call("source", "sun-valley.tcl")
+    root.tk.call("set_theme", "dark")
+
+    app = App(root)
+    app.pack(fill="both", expand=True)
+
+    # Set a minsize for the window, and place it in the middle
+    root.update()
+    root.minsize(root.winfo_width(), root.winfo_height())
+    x_cordinate = int((root.winfo_screenwidth() / 2) - (root.winfo_width() / 2))
+    y_cordinate = int((root.winfo_screenheight() / 2) - (root.winfo_height() / 2))
+    root.geometry("+{}+{}".format(x_cordinate, y_cordinate))
+
+    root.mainloop()
